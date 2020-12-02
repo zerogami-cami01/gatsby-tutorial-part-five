89067366f5a14ea55b964b2f0c8b7d5d2c8fec98
Zerogami01 <zerogamimusic@gmail.com>
2020-12-02 16:56:07 +0900

    add file

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
